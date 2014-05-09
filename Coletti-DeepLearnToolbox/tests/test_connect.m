19fefea660edba87023f768095261bb4518e4751
dracine <derek.racine@gmail.com>
2014-05-09 18:38:28 -0400

    autoencoder code for pretraining using rbms

--- Coletti-DeepLearnToolbox/tests/test_connect.m
+++ Coletti-DeepLearnToolbox/tests/test_connect.m
@@ -16,7 +16,7 @@ test_y  = double(test_y);
 test_x = normalize(test_x, mu, sigma);
 
 %% Neural net with dropout -- trained with parameters from DropConnect paper
-rand('state',0);
+rand('state', 0);
 nn = nnsetup([784 800 800 10]);
 
 nn.connectTraining = 1;
