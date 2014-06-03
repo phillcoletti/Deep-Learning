fcd8d0e2cafd8de7dbb0b820f7dda20f454f7875
phillcoletti <coletti.phillip@gmail.com>
2014-06-03 16:51:16 -0400

    removed old code

--- Coletti-DeepLearnToolbox/analysis/get_nn_allAvgPred.m
+++
@@ -1,9 +0,0 @@
-addpath('../data/')
-addpath('../NN/')
-addpath('../util')
-
-[ train_x, train_y, test_x, test_y ] = loadMNIST(  );
-
-nn_findAvgPredict1(200,test_x,test_y);
-nn_findAvgPredict2(200,test_x,test_y);
-nn_findAvgPredict3(200,test_x,test_y);
